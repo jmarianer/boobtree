427c5a52bf79eadad417e7bd5b5f1af906c82ab9
Joey Marianer <rmarianer@instructure.com>
2020-03-29 16:45:51 -0700

    Initial commit

---
+++ main_ui.ts
@@ -0,0 +1,49 @@
+var prev:any, next:any, transition_stage:any, next_offset:any, prev_offset:any;
+
+function done() {
+  transition_stage = 0
+  next_offset = next.offset()
+  prev_offset = prev.offset()
+
+  next.css('transition-property', 'all')
+  next.offset(next_offset)
+  next.offset(prev_offset)
+}
+
+function animend() {
+  // console.log(transition_stage)
+  switch(transition_stage) {
+    case 0:
+      prev.offset({left: window.innerWidth + 1000})
+      next.offset({left: -1000})
+      break
+    case 1:
+      next.css('transition-property', 'none')
+      next.offset(next_offset)
+      next.css('opacity', 0)
+      setTimeout(animend, 500)
+      break
+    case 2:
+      prev.css('transition-property', 'all')
+      prev.offset(prev_offset)
+      break
+    case 3:
+      next.css('transition-property', 'all')
+      next.css('opacity', 1)
+      next.offset(next_offset)
+      break
+    case 4:
+      prev.css('transition-property', 'none')
+      next.css('transition-property', 'none')
+      break
+  }
+  transition_stage++
+}
+
+$(() => {
+  prev = $('#previous')
+  next = $('#next')
+  prev.on('transitionend', animend)
+  next.on('transitionend', animend)
+  $('#done').click(done)
+});
---
+++ server.ts
@@ -0,0 +1,79 @@
+// tslint:disable:no-console
+import * as async from 'async';
+import * as browserify from 'browserify';
+import concat = require('concat-stream');
+import * as express from 'express';
+import * as fs from 'fs';
+import * as less from 'less';
+import * as path from 'path';
+import * as io from 'socket.io';
+
+// Helpers for serving Typescript and Less as JS and CSS.
+function serveJs(app: express.Express, url: string, tsFilename: string,
+                 callback: async.AsyncResultArrayCallback<string, string>) {
+  let errors = false;
+  console.log('Compiling ' + tsFilename);
+  browserify(tsFilename)
+    .plugin('tsify', { noImplicitAny: true, jsx: 'react' })
+    .bundle()
+    .on('error', (error: Error) => {
+      console.error(error.toString());
+      errors = true;
+    })
+    .pipe(concat((buf: Buffer) => {
+      if (errors) {
+        callback('Error compiling ' + tsFilename, null);
+      }
+      console.log('Finished compiling ' + tsFilename);
+      app.get(url, (req, res) => {
+        res.set('Content-Type', 'text/javascript');
+        res.send(buf.toString());
+      });
+      callback(null, null);
+    }));
+}
+
+function serveCss(app: express.Express, url: string, lessFilename: string,
+                  callback: async.AsyncResultArrayCallback<string, string>) {
+  console.log('Compiling ' + lessFilename);
+  async.waterfall([
+    async.apply(fs.readFile, lessFilename),
+    async.asyncify((data: Buffer) => data.toString()),
+    // TODO: Figure out why this is not the same as just "less.render".
+    (data: string, cb: (error: Less.RenderError, output: Less.RenderOutput) => void) => less.render(data, cb),
+    async.asyncify((data: Less.RenderOutput) => {
+      app.get(url, (req, res) => {
+        res.set('Content-Type', 'text/css');
+        res.send(data.css);
+      });
+      console.log('Finished compiling ' + lessFilename);
+      callback(null, null);
+    }),
+  ]);
+}
+
+// Main begins here.
+const app = express();
+
+async.parallel([
+  async.apply(serveJs, app, '/js/boobtree.js', 'main_ui.ts'),
+  async.apply(serveCss, app, '/style/style.css', 'style.less'),
+], (err, _results) => {
+  if (err) {
+    throw err;
+  }
+
+  app.get('/game/:game/user/:user', (request, response) => {
+    async.waterfall([
+      async.apply(fs.readFile, 'main_ui.html'),
+      async.asyncify((data: Buffer) => response.send(data.toString())),
+    ]);
+  });
+
+  let listener = app.listen(process.env.PORT, () => {
+    console.log('Your app is up');
+  });
+
+  let ioListener = io(listener);
+  ioListener.on('connection', (socket) => {});
+});
