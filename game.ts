6ad165b2f4643f0e5dba7cc3d9c76dcec02d9e76
Joey Marianer <rmarianer@instructure.com>
2020-04-01 09:42:15 -0700

    Transfer phrases (but not drawings) from one player to the next

--- game.ts
+++ game.ts
@@ -1,26 +1,51 @@
 export class Game {
   player_count : number;
-
-  players : { [user:string] : SocketIO.Socket };
+  current_round : number;
+  players : { [user:string] : {
+    socket : SocketIO.Socket;
+  }};
+  rounds : string[][];
+  player_names : string[];
 
   constructor(count : number) {
     this.player_count = count;
     this.players = {};
   }
   add_player(user : string, socket : SocketIO.Socket) {
-    this.players[user] = socket;
+    this.players[user] = { socket : socket};
     let all_names = Object.getOwnPropertyNames(this.players);
-    console.log(all_names);
     if (all_names.length == this.player_count) {
-      console.log('Starting game with players ' + all_names);
       this.start();
     }
   }
 
   start() {
-    let all_names = Object.getOwnPropertyNames(this.players);
-    for (let name of all_names) {
-      this.players[name].emit('start', all_names);
+    this.current_round = 0;
+    this.rounds = [[]];
+    this.player_names = Object.getOwnPropertyNames(this.players);
+
+    for (let player = 0; player < this.player_names.length; player++) {
+      let name = this.player_names[player];
+      let socket = this.players[name].socket;
+      socket.emit('start', name, this.player_names);
+      socket.on('phrase', (phrase : string) => {
+        this.add_phrase(player, phrase);
+      });
+    }
+  }
+
+  add_phrase(player : number, phrase : string) {
+    let round = this.rounds[this.current_round];
+    round[player] = phrase;
+    let players_done = round.filter((_) => true).length;
+    if (players_done == this.player_count) {
+      this.current_round++;
+      this.rounds.push([]);
+      for (let player = 0; player < this.player_names.length; player++) {
+        let name = this.player_names[player];
+        let socket = this.players[name].socket;
+        socket.emit('phrase', round[(player + 1) % this.player_names.length]);
+      }
     }
   }
 };
--- main_ui.ts
+++ main_ui.ts
@@ -1,21 +1,36 @@
 import * as $ from 'jquery';
 import * as io from 'socket.io-client';
 
+enum Mode { phrase, drawing }
+
 var
   prev : JQuery<HTMLElement>,
   next : JQuery<HTMLElement>,
   transition_stage : number,
   next_offset : JQuery.Coordinates,
-  prev_offset : JQuery.Coordinates;
+  prev_offset : JQuery.Coordinates,
+  current_mode: Mode;
 
 function done() {
-  transition_stage = 0;
   next_offset = next.offset();
   prev_offset = prev.offset();
 
+  $('#done').prop('disabled', true);
+  transition_stage = 0;
+  $('#instructions').css({visibility: 'hidden'});
+
   next.animate({top: prev_offset.top - next_offset.top}, () => {
     prev.css({visibility: 'hidden'});
-    next.animate({left: -1000 - next_offset.left});
+    next.animate({left: -1000 - next_offset.left}, () => {
+      next.css({opacity: 0});
+      next.offset(next_offset);
+
+      switch (current_mode) {
+        case Mode.phrase:
+          let val = $('#next-phrase').val();
+          socket.emit('phrase', val);
+      }
+    });
   });
 }
 
@@ -33,9 +48,31 @@ $(() => {
 });
 
 socket.on('start', () => { 
+  $('#instructions').text('Write a phrase here');
   $('#initial').show();
   $('#next-phrase').show();
+  current_mode = Mode.phrase;
 
   prev.animate({opacity: 1});
   next.animate({opacity: 1});
-})
+});
+
+socket.on('phrase', (phrase : string) => {
+  prev.offset({left: 1000});
+  prev.css({visibility: 'visible'});
+  prev.children().hide();
+  $('#previous-text').show();
+  $('#previous-text').text(phrase);
+  prev.animate({left: 0}, () => {
+    next.animate({opacity: 1});
+  });
+
+  next.children().hide();
+  $('#next-phrase').show();
+  $('#next-phrase').val('');
+
+  $('#instructions').css({visibility: 'visible'});
+  $('#instructions').text('Draw that phrase below');
+
+  $('#done').prop('disabled', false);
+});
